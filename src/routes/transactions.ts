1737b907cf0433281c51b3cb4555bd1b65590d38
Matheus Henrique <mattsilvahenrique102@gmail.com>
2023-02-20 11:00:25 -0300

    feat(transactions): create get transactions and summary routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,36 @@ import { z } from 'zod'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (req) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = getTransactionsParamsSchema.parse(req.params)
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return {
+      summary,
+    }
+  })
+
   app.post('/', async (req, res) => {
     const createTransactionsSchema = z.object({
       title: z.string(),
